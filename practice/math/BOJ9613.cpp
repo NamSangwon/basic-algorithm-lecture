8b45078f5ae7e9353b15b5918a34e3e20523338d
NamSangwon <nsw3265@naver.com>
2023-08-06 14:28:49 +0900

    수학 예제 문제 - 최대공약수의 합 구하기 (유클리드 호제법 주석 추가)

--- practice/math/BOJ9613.cpp
+++ practice/math/BOJ9613.cpp
@@ -5,6 +5,7 @@ using namespace std;
 int n, t;
 vector<int> v;
 
+// 최대 공약수(GCD) => GCD(12, 8) = GCD(8, 4) = GCD(4, 0) = 4 (유클리드 호제법)
 int gcd(int a, int b){
     if (b == 0) return a;
     return gcd(b, a%b);
