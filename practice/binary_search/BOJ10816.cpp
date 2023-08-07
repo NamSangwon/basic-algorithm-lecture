a05135ea0141de450cc4cc743a35cefaa121edc9
NamSangwon <nsw3265@naver.com>
2023-08-07 22:21:49 +0900

    이분 탐색 예제 문제 - 숫자 카드 개수 구하기 (힌트 제공)

---
+++ practice/binary_search/BOJ10816.cpp
@@ -0,0 +1,56 @@
+#include <bits/stdc++.h>
+
+// 숫자 카드의 개수 구하기 
+// [힌트] 해당 카드가 위치할 수 있는 최대 인덱스와 최소 인덱스의 차가 결과값이다.
+
+using namespace std;
+
+int n, m;
+vector<int> arr;
+
+// 그림을 그리면 이해하기 쉬움
+// 해당 카드가 위치할 수 있는 최소 인덱스
+int lower_idx(int target){
+    int st = 0;
+    int en = n;
+
+    while(st < en){
+        int mid = (st + en) / 2;
+        if (arr[mid] < target) st = mid + 1;
+        else en = mid;
+    }
+
+    return st;
+}
+// 해당 카드가 위치할 수 있는 최대 인덱스
+int upper_idx(int target){
+    int st = 0;
+    int en = n;
+
+    while(st < en){
+        int mid = (st + en) / 2;
+        if (arr[mid] <= target) st = mid + 1;
+        else en = mid;
+    }
+
+    return st;
+}
+
+int main(){
+    cin >> n;
+    for(int i = 0; i < n; i++) {
+        int num;
+        cin >> num;
+        arr.push_back(num);
+    }
+
+    sort(arr.begin(), arr.end());
+
+    cin >> m;
+    while(m--){
+        int num;
+        cin >> num;
+
+        cout << upper_idx(num) - lower_idx(num) << ' ';
+    }
+}
