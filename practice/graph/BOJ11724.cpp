65df6c8387b19eeb42b23ced2bb05b525957d6cb
NamSangwon <nsw3265@naver.com>
2023-08-13 18:10:38 +0900

    그래프 예제 문제 - 연결 요소의 개수 구하기 (클론 코딩)

---
+++ practice/graph/BOJ11724.cpp
@@ -0,0 +1,41 @@
+#include <bits/stdc++.h>
+
+// 연결 요소의 개수 구하기 (클론 코딩)
+
+using namespace std;
+
+int n, m;
+vector<int> board[1003]; // 인접리스트 형식으로 구현한 그래프
+bool visit[1003]; 
+
+int main(){
+    cin >> n >> m;
+
+    // 경로 입력 받기
+    while(m--){
+        int u, v;
+        cin >> u >> v;
+        board[u].push_back(v);
+        board[v].push_back(u);
+    }
+
+    int result = 0;
+    for(int i = 1; i <= n; i++){
+        if(visit[i]) continue;
+        result++;
+        queue<int> q;
+        q.push(i);
+        visit[i] = true;
+        while(!q.empty()){
+            int cur = q.front();
+            q.pop();
+            for(auto nxt : board[cur]){
+                if(visit[nxt]) continue;
+                q.push(nxt);
+                visit[nxt] = true;
+            }
+        }
+    }
+
+    cout << result;
+}
